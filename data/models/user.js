0af868d323c51cda6fa98a335630bb6e2c9799ce
Feng Chen <chenf@outlook.com>
2017-07-06 21:01:15 +0800

    make lint fail

--- data/models/user.js
+++ data/models/user.js
@@ -2,7 +2,7 @@ const Bookshelf = require('./bookshelf');
 const crypto = require('crypto');
 
 const User = Bookshelf.Model.extend({
-  tableName: 'user',
+  tableName: 'user'
 }, {
   register: (email, password) =>
     new User({ email, password_digest: crypto.createHash('md5').update(password).digest('hex') }).save(),
